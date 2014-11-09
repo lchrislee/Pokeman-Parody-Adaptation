23a5b9c242fc56a81dfff2031686dd90f9ce3342
Chris Lee <chrisl@usc.edu>
2014-11-09 07:46:50 -0800

    Quick change for demo.

--- src/EMPTY_TEST_CLASS_DELETE_ME.java
+++ src/EMPTY_TEST_CLASS_DELETE_ME.java
@@ -3,6 +3,9 @@ public class EMPTY_TEST_CLASS_DELETE_ME {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
+		for (int i = 0; i < 10; ++i){
+			System.out.println("CHEESE");
+		}
 	}
 
 }
