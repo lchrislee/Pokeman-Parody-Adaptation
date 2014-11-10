f00b032203fc1c363257498ba3ed535b65d80ae1
Alan Lau <alanlau@usc.edu>
2014-11-09 19:25:57 -0800

    i waz hre

--- src/EMPTY_TEST_CLASS_DELETE_ME.java
+++ src/EMPTY_TEST_CLASS_DELETE_ME.java
@@ -5,6 +5,7 @@ public class EMPTY_TEST_CLASS_DELETE_ME {
 		System.out.println("HELLO WORLD");
 		for (int i = 0; i < 10; ++i){
 			System.out.println("CHEESE");
+			System.out.println(" is amazing!");
 		}
 	}
 
