ddefb0c43004a16abb51416540245ed437c4492e
Chris Lee <chrisl@usc.edu>
2014-11-14 17:55:40 -0800

    Removed unused refs.

--- src/sidebarGUI/ItemListUI.java
+++ src/sidebarGUI/ItemListUI.java
@@ -3,8 +3,6 @@ package sidebarGUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
