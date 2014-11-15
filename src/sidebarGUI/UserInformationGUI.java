590b6da6c1e0378e3ef010f637d88a63b5805817
Chris Lee <chrisl@usc.edu>
2014-11-15 08:26:16 -0800

    Created UserInformationGUI. Did not make appearance but added the traversal code with the CardLayout.

---
+++ src/sidebarGUI/UserInformationGUI.java
@@ -0,0 +1,49 @@
+package sidebarGUI;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class UserInformationGUI extends JPanel {
+	private static final long serialVersionUID = -3880475893174044643L;
+	private CardLayout switcher;
+	private SideBarMenuAdapter parent;
+	private JButton btnBack;
+
+	public UserInformationGUI(){
+		
+	}
+	
+	public UserInformationGUI(CardLayout c, SideBarMenuAdapter p){
+		switcher = c;
+		parent = p;
+		createGUI();
+		addListeners();
+	}
+	
+	private void createGUI(){
+		btnBack = new JButton("Back");
+	}
+	
+	private void addListeners(){
+		btnBack.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				switcher.show(parent, parent.MENUNAME);
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		JFrame temp = new JFrame();
+		temp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		temp.setSize(300, 350);
+		temp.add(new UserInformationGUI());
+		temp.setVisible(true);
+	}
+
+}
