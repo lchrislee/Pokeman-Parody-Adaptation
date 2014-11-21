11524e46fc655e7b77e0cde54f7e3485780ec907
Alan Lau <alanlau@usc.edu>
2014-11-21 15:38:18 -0800

    Added potion stuff..dunno why I didn't do this earlier.

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -7,10 +7,12 @@ public class Potion implements Item {
 	@Override
 	public void use(Pokemon p) {
 		// TODO Auto-generated method stub
-		if(p.isConscious())
-			System.out.println("Recover a certain amount of health");
+		if(p.isConscious() && p.getHealth()!=p.getMaxHealth()){
+			int newHealth = (int) (p.getHealth()*1.2);
+			p.setHealth(newHealth);
 		
-		
+			
+		}
 	}
 
 }
