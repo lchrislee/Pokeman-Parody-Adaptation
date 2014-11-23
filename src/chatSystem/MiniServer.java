64e7ffd12d7b4cd50930b94c61a21abd4eb30ed5
Priscilla Rodriguez <plrodrig@usc.edu>
2014-11-23 14:13:26 -0800

    Deleted networkThread

--- src/chatSystem/MiniServer.java
+++ src/chatSystem/MiniServer.java
@@ -1,101 +1,91 @@
 package chatSystem;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-import sidebarGUI.ChatGUI;
-
-
-public class MiniServer implements Runnable{
+public class MiniServer implements Runnable {
 	Socket socket = null;
 	private Scanner input;
 	private PrintWriter output;
 	String message = "";
-	//Socket socket = null;
-	//ChatGUI myGA;
-	//BufferedReader myBR;
 
-	public MiniServer(Socket socket){
+	public MiniServer(Socket socket) {
 		this.socket = socket;
 
 	}
-	public void CheckConnection() throws IOException{
-		if(!socket.isConnected()){
-			for(int i = 0; i < ChatServer.connectionArray.size(); i++){
-				if(ChatServer.connectionArray.get(i) == socket){
+
+	public void CheckConnection() throws IOException {
+		if (!socket.isConnected()) {
+			System.out.println("NO LONGER CONNECTED");
+			for (int i = 0; i < ChatServer.connectionArray.size(); i++) {
+				if (ChatServer.connectionArray.get(i) == socket) {
 					ChatServer.connectionArray.remove(i);
 				}
 			}
-			
-			for(int i = 0; i < ChatServer.connectionArray.size(); i++){
-				Socket tempSock = (Socket) ChatServer.connectionArray.get(i-1);
+
+			for (int i = 0; i < ChatServer.connectionArray.size(); i++) {
+				Socket tempSock = (Socket) ChatServer.connectionArray
+						.get(i - 1);
 				PrintWriter output = new PrintWriter(tempSock.getOutputStream());
-				output.println(tempSock.getLocalAddress().getHostName() + "disconnected");
+				output.println(tempSock.getLocalAddress().getHostName()
+						+ "disconnected");
 				output.flush();
-				System.out.println(tempSock.getLocalAddress().getHostName() + "disconnected");
+				System.out.println(tempSock.getLocalAddress().getHostName()
+						+ "disconnected");
 			}
 		}
 	}
-	public void run(){
+
+	public void run() {
 		try {
 			input = new Scanner(socket.getInputStream());
 			output = new PrintWriter(socket.getOutputStream());
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
-		while(true){			
+
+		while (true) {
 			try {
-				
+
 				CheckConnection();
-				
-				if(!input.hasNext()){
-				
-				continue;
+
+				if (!input.hasNext()) {
+					continue;
 				}
-				
+
 				message = input.nextLine();
-				
+
+				System.out.println(ChatServer.currentUsers);
+
+				// if (message.contains("#FROM: ") && message.contains("#TO: ")
+
 				System.out.println("Client message: " + message);
 				System.out.println("ConnectionList size:");
 				System.out.println(ChatServer.connectionArray.size());
-				/*for(int i  = 0; i < ChatServer.connectionArray.size()-1; i++){
-						
-						Socket tempSock = (Socket) ChatServer.connectionArray.get(i);
-						PrintWriter output = new PrintWriter(tempSock.getOutputStream());
-						output.println(message);
-						output.flush();
-						System.out.println("Sent to: " + tempSock.getLocalAddress().getHostName());
-				}*/
-				
+
 				for (Socket tempSock : ChatServer.connectionArray) {
-					//tempSock = (Socket) ChatServer.connectionArray.get(i);
-					PrintWriter output = new PrintWriter(tempSock.getOutputStream());
+					PrintWriter output = new PrintWriter(
+							tempSock.getOutputStream());
 					output.println(message);
 					output.flush();
-					System.out.println("Sent to: " + tempSock.getLocalAddress().getHostName());
+					System.out.println("Sent to: "
+							+ tempSock.getLocalAddress().getHostName());
 				}
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} /*finally{
+			} finally {
 				try {
 					socket.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}*/
-			//read input and process here
-			//read message from the chatgui
-			//String msg = myBR.readLine(); //waits here cant stick this in gui
-			//myGA.readMessage(msg);
-		//	myGA.readMessage("oo");
-			
+			}
+
 		}
-		//implement methods here
+
 	}
 }
