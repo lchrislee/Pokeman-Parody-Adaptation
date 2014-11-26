796a6335084eef880005cd9edd72ea5b5c5faa6d
Alan Lau <alanlau@usc.edu>
2014-11-26 13:46:29 -0800

    Removed stuff from battle server and server. Added switch for battle

--- src/Battle/Battle.java
+++ src/Battle/Battle.java
@@ -2,28 +2,67 @@ package Battle;
 
 import java.util.concurrent.RecursiveTask;
 
+import dataStore.Player;
+import dataStore.Pokemon;
+
 public class Battle extends RecursiveTask<Boolean> {
 	String input;
 	private boolean player1Quit;
 	private boolean player2Quit;
 	private int firstPlayerToQuit;
+	private int firstPlayerToSwitch;
 	private boolean player1Selected;
 	private boolean player2Selected;
+	private boolean player1Switch;
+	private boolean player2Switch;
+	
+	private static int PLAYERONE = 1;
+	private static int PLAYERTWO = 2;
+	
+	private Player p1;
+	private Player p2;
+	
 	
 	public Battle(){
 		setTurnVariables();
 	}
 	
+	public Battle(Player p1, Player p2){
+		this.p1 = p1;
+		this.p2 = p2;
+		setTurnVariables();
+	}
+	
 	public static void main(String[] args) {
+		Player p1;
+		Player p2;
+		
+		
 		Battle b = new Battle();
 		b.input = "Attack_Tackle|50~P1 P2";
 		System.out.println(b.compute());
 		b.input = "Attack_Tackle|50~P2 P1";
 		System.out.println(b.compute());
+	
+		System.out.println("<<<<<<<");
+		b.input = "Sw_1-Swagasaur";
+		System.out.println(b.compute());
+		b.input = "Sw_2-Pikachu";
+		System.out.println(b.compute());
+		System.out.println("<<<<<<<");
+		
 		b.input = "Surrender_P1";
 		System.out.println(b.compute());
 		b.input = "Surrender_P2";
 		System.out.println(b.compute());
+	
+		System.out.println(">>>>>>");
+		b.input = "Sw_1-Swagasaur";
+		System.out.println(b.compute());
+		b.input = "Sw_2-Pikachu";
+		System.out.println(b.compute());
+		System.out.println(">>>>>>");
+	
 	}
 
 	@Override
@@ -32,7 +71,14 @@ public class Battle extends RecursiveTask<Boolean> {
 		if (input.charAt(0) == 'A'){
 			attack(input);
 			output = true;
-		}else if (input.indexOf("Su") == 0){
+		}
+		
+		else if(input.indexOf("Sw") == 0){
+			swap(input);
+			output = true;
+		}
+		
+		else if (input.indexOf("Su") == 0){
 			surrender(input);
 			output = false;
 		}
@@ -92,4 +138,63 @@ public class Battle extends RecursiveTask<Boolean> {
 			player2Selected = true;
 		}
 	}
+	
+	private int turnOrder(){//returns who goes first
+		Pokemon p1Poke = p1.getCurrentPokemon();
+		Pokemon p2Poke = p2.getCurrentPokemon();
+		
+		if(p1Poke.getSpeed() > p2Poke.getSpeed())
+			return PLAYERONE;
+	
+		else if(p1Poke.getSpeed() == p2Poke.getSpeed()){
+			
+			if(p1Poke.getRarity() > p2Poke.getRarity())
+				return PLAYERONE;
+			
+			else if(p1Poke.getRarity() == p2Poke.getRarity()){
+				int num = (int)Math.random()%2;
+				if(num == 0)
+					return PLAYERTWO;
+				
+				return PLAYERONE;
+			}
+			
+			return PLAYERTWO;
+		}
+				
+		return PLAYERTWO;
+	}
+	
+	private synchronized void swap(String input){//string will be number-pokemonname... eg: 1-Lickister  or 2-Beetwo
+			if(!(player1Quit || player2Quit)){//will run only if nobody's quit yet
+				int digitIndex = input.indexOf("_")+1;
+				int playerIndex = Integer.parseInt(input.substring(digitIndex,digitIndex+1));
+				
+				String pokemonName = input.substring(input.indexOf("-")+1,input.length());
+				
+				if (playerIndex == 1){
+					player1Switch = true;
+					player2Switch = false;
+					firstPlayerToSwitch = PLAYERONE;
+				}
+				
+				else{
+					player2Switch = true;
+					player1Switch = false;
+					
+					firstPlayerToSwitch = PLAYERTWO;
+				}
+				
+				System.out.println("Player " + firstPlayerToSwitch + " wants to switch to " + pokemonName);
+				//can reset turn order later
+			}
+			
+			else{
+				System.out.println("NO SWTICHING! WE QUIT ");
+			}
+		
+	}
+	
+	
+	
 }
