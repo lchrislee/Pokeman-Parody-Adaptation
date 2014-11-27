58435e6b0b4cf5331e8937685e11e5bd07973c8d
Priscilla Rodriguez <plrodrig@usc.edu>
2014-11-26 17:10:12 -0800

    main

--- src/client/clientGUI/GUI.java
+++ src/client/clientGUI/GUI.java
@@ -1,5 +1,29 @@
 package client.clientGUI;
 
-public class GUI {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame{
+	//800 x 600
+	public GUI(){
+		createGUI();
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(500,500);
+		setVisible(true);
+	}
+	private void createGUI(){
+		setLayout(new BorderLayout());
+		JPanel sidebarPnl = new JPanel();
+		add(sidebarPnl, BorderLayout.EAST);
+		JPanel commandCenterPnl =new JPanel();
+		add(commandCenterPnl, BorderLayout.SOUTH);
+		JPanel gamePnl = new JPanel();
+		add(gamePnl, BorderLayout.NORTH);
+		
+	}
+	public static void main(String[] args){
+		GUI p = new GUI();
+	}
 }
