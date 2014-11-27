53f2910132053a1279b05c48a00bcdc9b534ae31
Chris Lee <chrisl@usc.edu>
2014-11-26 16:03:25 -0800

    Added warning suppressions.

--- src/client/sidebarGUI/ChatGUI.java
+++ src/client/sidebarGUI/ChatGUI.java
@@ -29,6 +29,7 @@ public class ChatGUI extends JPanel {
 	private final String[] groupNames = {"ALL", "P1 Only", "P2 Only", "P3 Only", "P4 Only", "P1 & P2", "P1 & P3", "P1 & P4", "P2 & P3", "P2 & P4", "P3 & P4"};
 	public static JTextPane txeaAllMessages;
 	private JTextField txFdCurrentText;
+	@SuppressWarnings("rawtypes")
 	private JComboBox groups;
 	private JButton btnSend;
 	private String chatSelected = groupNames[0];
@@ -45,6 +46,7 @@ public class ChatGUI extends JPanel {
 		Connect(address);
 	}
 	
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	private void createGUI(){
 		setLayout(new BorderLayout());
 		setPreferredSize(new Dimension(300, 250));
@@ -93,9 +95,9 @@ public class ChatGUI extends JPanel {
 		txFdCurrentText.getActionMap().put("ENTER", enterPressed);
 		
 		groups.addActionListener(new ActionListener(){
+			@SuppressWarnings("rawtypes")
 			@Override
 			public void actionPerformed(ActionEvent source) {
-				@SuppressWarnings("unchecked")
 				String selected = ((JComboBox)source.getSource()).getSelectedItem().toString();
 				if (selected.equals(chatSelected))
 					return;
