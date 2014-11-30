0580b4b9a6d7aa6ace93dbfdbf6fcec2ed927412
Chris Lee <chrisl@usc.edu>
2014-11-30 00:21:48 -0800

    Removed some unnecessary references.

--- src/client/clientGUI/sidebarGUI/SideBar.java
+++ src/client/clientGUI/sidebarGUI/SideBar.java
@@ -2,15 +2,12 @@ package client.clientGUI.sidebarGUI;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SideBar extends JPanel {
 	private static final long serialVersionUID = 5707609256750142398L;
 
 	public SideBar(String address){
-	
 		createGUI(address);
 	}
 	
@@ -19,5 +16,4 @@ public class SideBar extends JPanel {
 		add(new SideBarMenuAdapter());
 		add(new ChatGUI(address), BorderLayout.SOUTH);
 	}
-	
 }
