b9feb6b1dced15769ea0ab0a0b9fb9b6393648b0
Jiwoo Kim <jiwookim@usc.edu>
2014-11-30 00:34:03 -0800

    bracket and battlescreen update

--- src/client/clientGUI/Waiting/FirstBracket.java
+++ src/client/clientGUI/Waiting/FirstBracket.java
@@ -22,7 +22,6 @@ public class FirstBracket extends JPanel implements ActionListener {
 	int timerCountSpeed = 1;
 	public boolean done = false;
 	ArrayList<NetworkPlayer> players;
-	
 	private int selector;
 	private int winner1, winner2, winner3;
 	private boolean drawWinner1 = false;
@@ -66,38 +65,47 @@ public class FirstBracket extends JPanel implements ActionListener {
 	}
 		
 	public void paintComponent(Graphics g) {
+		boolean dummyOn = false;
+		if (players.size() == 0) dummyOn = true;
 		g.setColor(Color.white);
 		g.fillRect(0, 0, 800, 600);
 		
 		g.setColor(Color.black);
 		int i = 0;
 		
-		String firstPlayerName = "Player 0"; i++;
-		Image firstImage = Toolkit.getDefaultToolkit().getImage("res/Character_sprites/Dolan_normal.png");
+		String firstPlayerName = "", secondPlayerName = "", thirdPlayerName = "", fourthPlayerName = "";
+		Image firstPlayerImage = null, secondPlayerImage = null, thirdPlayerImage = null, fourthPlayerImage = null;
+		String winner1Name = "", winner2Name= "", winner3Name = "";
 		
-		/* use the commented code for real players and not dummies */
-		/* String firstPlayerName = players.get(i).getName();
-		 * ImageIcon firstPlayerImageIcon = players.get(i).getCurrentSprite();
-		 * Image firstPlayerImage = firstPlayerImageIcon.getImage();
-		 * i++; */
-		String secondPlayerName = "Player 1";
-		/* String secondPlayerName = players.get(i).getName(); */
-		/* String secondPlayerName = players.get(i).getName();
-		 * ImageIcon secondPlayerImageIcon = players.get(i).getCurrentSprite();
-		 * Image secondPlayerImage = secondPlayerImageIcon.getImage();
-		 * i++; */
-		String thirdPlayerName = "Player 2";
-		/* String thirdPlayerName = players.get(i).getName(); */
-		/* String thirdPlayerName = players.get(i).getName();
-		 * ImageIcon thirdPlayerImageIcon = players.get(i).getCurrentSprite();
-		 * Image thirdPlayerImage = thirdPlayerImageIcon.getImage();
-		 * i++; */
-		String fourthPlayerName = "Player 3";
-		/* String fourthPlayerName = players.get(i).getName(); */
-		/* String fourthPlayerName = players.get(i).getName();
-		 * ImageIcon fourthPlayerNameImageIcon = players.get(i).getCurrentSprite();
-		 * Image fourthPlayerNameImage = fourthPlayerNameImageIcon.getImage();
-		 * i++; */
+		if (dummyOn) {
+			firstPlayerName = "Player 0"; i++;
+			firstPlayerImage = Toolkit.getDefaultToolkit().getImage("res/Character_sprites/Dolan_normal.png");
+			secondPlayerImage = Toolkit.getDefaultToolkit().getImage("res/Character_sprites/Dolan_normal.png");
+			thirdPlayerImage = Toolkit.getDefaultToolkit().getImage("res/Character_sprites/Dolan_normal.png");
+			fourthPlayerImage = Toolkit.getDefaultToolkit().getImage("res/Character_sprites/Dolan_normal.png");
+			secondPlayerName = "Player 1";
+			thirdPlayerName = "Player 2";
+			fourthPlayerName = "Player 3";
+		}
+		
+		else {
+			firstPlayerName = players.get(i).getName();
+			ImageIcon firstPlayerImageIcon = players.get(i).getCurrentSprite();
+			firstPlayerImage = firstPlayerImageIcon.getImage();
+			i++;
+			secondPlayerName = players.get(i).getName();
+			ImageIcon secondPlayerImageIcon = players.get(i).getCurrentSprite();
+			secondPlayerImage = secondPlayerImageIcon.getImage();
+			i++; 
+			thirdPlayerName = players.get(i).getName();
+			ImageIcon thirdPlayerImageIcon = players.get(i).getCurrentSprite();
+			thirdPlayerImage = thirdPlayerImageIcon.getImage();
+			i++; 
+			fourthPlayerName = players.get(i).getName(); 
+			ImageIcon fourthPlayerNameImageIcon = players.get(i).getCurrentSprite();
+			fourthPlayerImage = fourthPlayerNameImageIcon.getImage();
+			i++; 
+		}
 		g.setFont(new Font("Arial",Font.BOLD, 20));
 		
 		g.fillRect(100,300,5,100);
@@ -107,13 +115,13 @@ public class FirstBracket extends JPanel implements ActionListener {
 		g.setColor(Color.BLUE);
 		// giving dummies for now
 		g.drawString(firstPlayerName,70,420);
-		g.drawImage(firstImage,10,420,this);
+		g.drawImage(firstPlayerImage,10,420,this);
 		g.drawString(secondPlayerName,270,420);
-		g.drawImage(firstImage,210,420,this);
+		g.drawImage(secondPlayerImage,210,420,this);
 		g.drawString(thirdPlayerName,470,420);
-		g.drawImage(firstImage,400,420,this);
+		g.drawImage(thirdPlayerImage,400,420,this);
 		g.drawString(fourthPlayerName,670,420);
-		g.drawImage(firstImage,600,420,this);
+		g.drawImage(fourthPlayerImage,600,420,this);
 		
 		g.setColor(Color.BLACK);
 		
@@ -130,23 +138,38 @@ public class FirstBracket extends JPanel implements ActionListener {
 		
 		g.setFont(new Font("Arial",Font.BOLD, 20));
 		g.setColor(Color.ORANGE);
-		// dummy name for winner 1
-		String winner1Name = "Winner 1";
-		/* String winner1Name = players.get(winner1).getName();*/
+		
+		if (dummyOn) {
+			winner1Name = "Winner 1";
+		}
+		
+		else {
+			winner1Name = players.get(winner1).getName();
+		}
+		
 		if (drawWinner1) {
 			g.drawString(winner1Name,210,195);
 		}
 		
-		String winner2Name = "Winner 2";
-		/* String winner2Name = players.get(winner2).getName();*/
+		if (dummyOn) {
+			winner2Name = "Winner 2";
+		}
+		else {
+			winner2Name = players.get(winner2).getName();
+		}
 		if (drawWinner2) {
 			g.drawString(winner2Name,500,195);
 		}
 		
 		g.setFont(new Font("Arial",Font.BOLD, 35));
 		g.setColor(Color.RED);
-		String winner3Name = "Winner 3";
-		/* String winner3Name = players.get(winner3).getName();*/
+		
+		if (dummyOn) {
+			winner3Name = "Winner 3";
+		}
+		else {
+			winner3Name = players.get(winner3).getName();
+		}
 		if (drawWinner3) {
 			g.drawString(winner3Name,335,60);
 		}
