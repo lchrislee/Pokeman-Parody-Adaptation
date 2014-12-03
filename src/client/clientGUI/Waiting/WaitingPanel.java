deb136ec21e04eb8c766b3423035d84af27705f1
Jiwoo Kim <jiwookim@usc.edu>
2014-12-03 11:38:35 -0800

    fwefwe

--- src/client/clientGUI/Waiting/WaitingPanel.java
+++ src/client/clientGUI/Waiting/WaitingPanel.java
@@ -9,6 +9,11 @@ import java.awt.Toolkit;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -20,7 +25,23 @@ import client.clientGUI.LoginScreen.LoginScreen;
 public class WaitingPanel extends JPanel {
 	private JLabel waitingLabel;
 	Image sexypodImage = null;
+	public Clip clip = null;
+	AudioInputStream inputStream;
 	public WaitingPanel() {
+		if (clip==null) {
+			try {
+				inputStream = AudioSystem.getAudioInputStream(getClass().getResource("/careless_whisper_saxophone_instrumental_2_.wav"));
+				clip = AudioSystem.getClip();
+				clip.open(inputStream);
+				clip.start();
+			} catch(LineUnavailableException lue) {
+				System.out.println("fail reading music");
+			} catch(IOException ioe) {
+				System.out.println("fail reading music 2");
+			} catch(UnsupportedAudioFileException uafe) {
+				System.out.println("fail reading music 3");
+			}
+		}
 		try {
 			sexypodImage = ImageIO.read(getClass().getResource("/Pokemon_sprites/Sexy-apod_left_tr.png"));
 		} catch(IOException ioe) {
@@ -33,12 +54,16 @@ public class WaitingPanel extends JPanel {
 	}
 	
 	public void createWaitingGUI() {
+		
 		waitingLabel = new JLabel("Waiting for other players . . .");
 		waitingLabel.setFont(new Font("Arial",Font.PLAIN, 30));
 		final ImageIcon sexypodImageIcon = new ImageIcon(sexypodImage);
 		JLabel sexypodLabel = new JLabel(sexypodImageIcon);
 		add(waitingLabel);
 		add(sexypodLabel);
+	}
+	public void stop(){
+		clip.stop();
 	}
 	public static void main(String args[]) {
 		WaitingPanel w = new WaitingPanel();
